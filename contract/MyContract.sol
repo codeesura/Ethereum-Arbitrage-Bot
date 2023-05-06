bf8543173634dd61338d7f7bc2ebdac59ce280f6
codeesura <120671243+codeesura@users.noreply.github.com>
2023-05-06 13:35:25 +0300

    Update MyContract.sol
    unused library

--- contract/MyContract.sol
+++ contract/MyContract.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 interface IUniswapV2Router {
     function swapExactTokensForETH(uint amountIn,uint amountOutMin,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts);
     function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
